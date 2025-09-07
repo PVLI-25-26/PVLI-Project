84dacf3822a2230e4f9fca28fbb7ce4d4b918878
Dekros <clearsky3210@gmail.com>
2025-09-07 10:05:25 +0200

    feat: update gameplay config

--- src/configs/gameplayConfig.js
+++ src/configs/gameplayConfig.js
@@ -1,7 +1,7 @@
 export const platformConfig = [
   { x: 400, y: 580, width: 800, height: 40, color: 0xaef04a },
-  { x: 400, y: 400, width: 200, height: 20, color: 0x6d797a },
-  { x: 600, y: 300, width: 200, height: 20, color: 0x6d797a },
+  { x: 400, y: 500, width: 200, height: 20, color: 0x6d797a },
+  { x: 600, y: 400, width: 200, height: 20, color: 0x6d797a },
 ];
 
 export const playerConfig = {
@@ -11,9 +11,9 @@ export const playerConfig = {
   height: 60,
   color: 0xff00ff,
   speed: 400,
-  jumpSpeed: -450,
+  jumpSpeed: -650,
 };
 
 export const physicsConfig = {
-  gravityY: 500,
+  gravityY: 1500,
 };
