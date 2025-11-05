d12f638fbead53e8a93ce6557da4cba7e6cb6fba
aisasi <aisasi505@gmail.com>
2025-11-05 18:42:16 +0100

    feat(): make camera rotate with delta

--- src/js/components/PlayerController.js
+++ src/js/components/PlayerController.js
@@ -59,12 +59,12 @@ export class PlayerControllerComponent extends BaseControllerComponent {
         // Don't allow both rotation keys at the same time because 
         //      it is stupid and it will call many events unnecessarily (improves performance)
         if(this.keys.rotCamRight.isDown && !this.keys.rotCamLeft.isDown) {
-            this.camera.rotation -= 0.05;
+            this.camera.rotation -= 0.002*delta; // Add rotation speed to config
             // Emit event with new rotation and precalculated cos/sin for performance
             EventBus.emit('cameraRotated', this.camera.rotation, Math.cos(-this.camera.rotation), Math.sin(-this.camera.rotation));
         }
         if(this.keys.rotCamLeft.isDown && !this.keys.rotCamRight.isDown) {
-            this.camera.rotation += 0.05;
+            this.camera.rotation += 0.002*delta; // Add rotation speed to config
             // Emit event with new rotation and precalculated cos/sin for performance
             EventBus.emit('cameraRotated', this.camera.rotation, Math.cos(-this.camera.rotation), Math.sin(-this.camera.rotation));
         }
