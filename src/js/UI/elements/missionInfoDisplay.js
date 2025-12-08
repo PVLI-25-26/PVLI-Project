74348420fbafab4a951973fed31bda6b270b5254
Adrian Isasi <aisasi505@gmail.com>
2025-12-08 23:30:34 +0100

    docs: adds more docs to files with missing docs

--- src/js/UI/elements/missionInfoDisplay.js
+++ src/js/UI/elements/missionInfoDisplay.js
@@ -1,30 +1,58 @@
 import Colors from "../../../configs/colors-config";
 
+/**
+ * Visual representation of a mission entry showing summary and progress.
+ *
+ * Displays mission summary text, a progress suffix computed from the mission's
+ * completeConditions and a green strike rectangle used to mark completion.
+ *
+ * @extends Phaser.GameObjects.Container
+ */
 export class MissionInfoDisplay extends Phaser.GameObjects.Container{
+    /**
+     * Create a MissionInfoDisplay.
+     *
+     * @param {Phaser.Scene} scene - Scene to which this display will be added.
+     * @param {number} x - X position of the container.
+     * @param {number} y - Y position of the container.
+     * @param {Object} mission - Mission object containing summary and completeConditions.
+     * @param {boolean} isCompleted - If true, the mission is rendered as already completed.
+     */
     constructor(scene, x, y, mission, isCompleted){
         super(scene, x, y);
 
         this.mission = mission;
 
+        // Create mission summary text
         this.missionSummary = this.scene.add.text(0, 0, "", {
             fontFamily: 'MicroChat',
             fontSize: 10,
             color: Colors.White
         });
 
+        // Update mission text depending on progress
         this.updateProgress();  
 
+        // Add text to container
         this.add(this.missionSummary);
 
+        // Create rectangle to cross mission when complete
         this.rect = this.scene.add.rectangle(this.missionSummary.x-this.missionSummary.width, this.missionSummary.y-this.missionSummary.height/2, 0, this.missionSummary.height/4, Colors.GreenHex);
         this.add(this.rect);
 
+        // If mission is complete set width of rectangle to cross mission text
         if(isCompleted){
             this.rect.width = this.missionSummary.width;
         }
     }
 
+    /**
+     * Animate and mark the mission as completed by expanding the strike rectangle.
+     *
+     * @returns {void}
+     */
     completeMission(){
+        // Cross mission text with rectangle
         this.scene.tweens.add({
             targets: this.rect,
             width: this.missionSummary.width,
@@ -33,7 +61,14 @@ export class MissionInfoDisplay extends Phaser.GameObjects.Container{
         })
     }
 
+    /**
+     * Update the missionSummary text to include current progress for each completion condition.
+     * The function appends "progress/target" fragments according to each condition's operator.
+     *
+     * @returns {void}
+     */
     updateProgress(){
+        // set mission text depending on progress and operator used
         this.missionSummary.text = this.mission.summary;
         this.mission.completeConditions.forEach(cond => {
             switch(cond.op){
@@ -50,7 +85,13 @@ export class MissionInfoDisplay extends Phaser.GameObjects.Container{
         });
     }
 
+    /**
+     * Fade out and destroy this display instance.
+     *
+     * @returns {void}
+     */
     remove(){
+        // Destroy object after fading out
         this.scene.tweens.add({
             targets: this,
             alpha: 0,
--- src/js/entities/BuffParticleEmitter.js
+++ src/js/entities/BuffParticleEmitter.js
@@ -2,32 +2,38 @@ export class BuffParticleEmitter {
     constructor(scene, target, texture, duration, speed, width, frameMin = 0, frameMax = 0){
         this.scene = scene;
         
+        // Create emitter
         this.emitter = scene.add.particles(0, 0, texture, {
             speedY: {
                 onEmit: (particle, key, t, value)=> {
+                    // Make particles go up depending on camera rotation
                     const camRot = scene.cameras.main.rotation;
                     return -speed*Math.cos(camRot)
                 }
             },
             speedX: {
                 onEmit: (particle, key, t, value)=> {
+                    // Make particles go up depending on camera rotation
                     const camRot = scene.cameras.main.rotation;
                     return -speed*Math.sin(camRot)
                 }
             },
             x:{
                 onEmit: (particle, key, t, value)=> {
+                    // Choose random horizontal pos (horizontal from camera perspective) in target given
                     const camRot = scene.cameras.main.rotation;
                     return ((Math.random()*2-1)*width)*Math.cos(camRot) + target.x;
                 }
             },
             y:{
                 onEmit: (particle, key, t, value)=> {
+                    // Choose random horizontal pos (horizontal from camera perspective) in target given
                     const camRot = scene.cameras.main.rotation;
                     return ((Math.random()*2-1)*width)*Math.sin(camRot) + target.y;
                 }
             },
             rotate: {
+                // Rotate particle to camera rotation
                 onEmit: (particle, key, t, value)=> {
                     const camRot = scene.cameras.main.rotation;
                     return -camRot*180/Math.PI;
